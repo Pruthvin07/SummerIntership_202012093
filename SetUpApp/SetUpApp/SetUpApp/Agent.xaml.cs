c9242bac517878e49bb3ae23a125a3d6923cb5ad
Pruthvin07 <pruthvinjhaveri@gmail.com>
2021-07-16 12:42:25 +0530

    Agent Page

---
+++ SetUpApp/SetUpApp/SetUpApp/Agent.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SetUpApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Agent : ContentPage
+    {
+        public Agent()
+        {
+            InitializeComponent();
+        }
+    }
+}
